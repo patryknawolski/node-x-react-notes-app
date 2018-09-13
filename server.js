34d8df4783e851547d57f90a6ffe4e50ccb98104
patryknawolski <patryk.nawolski@netguru.co>
2018-09-13 14:17:30 +0200

    Prepare for Heroku deployment

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  // Serve static files from the React frontend app
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Anything that doesn't match the above, send back index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  });
+}
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
